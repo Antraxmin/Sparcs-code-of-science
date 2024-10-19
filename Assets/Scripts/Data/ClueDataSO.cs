01f1f8642d24b37e924f274fc82a41acdad03726
Antraxmin <antraxmin@naver.com>
2024-10-20 02:32:02 +0900

    update: 단서 및 퀴즈 UI 업데이트

--- Assets/Scripts/Data/ClueDataSO.cs
+++ Assets/Scripts/Data/ClueDataSO.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class ClueDataSO : ScriptableObject
 {
     public string clueName;         // 단서 이름
-    public int clueNumber;          // 단서 번호
+    public int clueID;              // 단서 고유 ID (이것이 필요합니다)
     public string clueDescription;  // 단서 설명
     public string quizQuestion;     // 퀴즈 문제
     public string[] choices = new string[4];  // 4개의 선택지
     public int correctAnswerIndex;  // 정답 인덱스 (0~3)
     public string explanation;      // 해설
     public string[] dialogues = new string[5];  // 5개의 대화 내용
-    public Sprite clueIcon;         
+    public Sprite clueIcon;         // 단서 아이콘
 }
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,101 +1,8 @@
-// using UnityEngine;
-// using UnityEngine.EventSystems;
-
-// public class PlayerMovement : MonoBehaviour
-// {
-//     public float moveSpeed = 2f;
-//     private Rigidbody2D rb;
-//     private SpriteRenderer spriteRenderer;
-//     private Vector2 movement;
-
-//     private bool isMovingUp = false;
-//     private bool isMovingDown = false;
-//     private bool isMovingLeft = false;
-//     private bool isMovingRight = false;
-
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         spriteRenderer = GetComponent<SpriteRenderer>();
-//     }
-
-//     void Update()
-//     {
-//         movement = Vector2.zero;
-
-//         if (isMovingUp)
-//         {
-//             movement.y = 1;
-//         }
-//         else if (isMovingDown)
-//         {
-//             movement.y = -1;
-//         }
-
-//         if (isMovingLeft)
-//         {
-//             movement.x = -1;
-//             spriteRenderer.flipX = true; 
-//         }
-//         else if (isMovingRight)
-//         {
-//             movement.x = 1;
-//             spriteRenderer.flipX = false; 
-//         }
-//     }
-
-//     void FixedUpdate()
-//     {
-//         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-//     }
-
-//     public void OnMoveUpDown()
-//     {
-//         isMovingUp = true;
-//     }
-
-//     public void OnMoveUpUp()
-//     {
-//         isMovingUp = false;
-//     }
-
-//     public void OnMoveDownDown()
-//     {
-//         isMovingDown = true;
-//     }
-
-//     public void OnMoveDownUp()
-//     {
-//         isMovingDown = false;
-//     }
-
-//     public void OnMoveLeftDown()
-//     {
-//         isMovingLeft = true;
-//     }
-
-//     public void OnMoveLeftUp()
-//     {
-//         isMovingLeft = false;
-//     }
-
-//     public void OnMoveRightDown()
-//     {
-//         isMovingRight = true;
-//     }
-
-//     public void OnMoveRightUp()
-//     {
-//         isMovingRight = false;
-//     }
-// }
-
-
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 0.5f;
+    public float moveSpeed = 2f;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Vector2 movement;
@@ -104,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isMovingDown = false;
     private bool isMovingLeft = false;
     private bool isMovingRight = false;
+    private bool canMoveUp = false;  // 사다리 안에 있을 때만 위로 이동 가능
 
     void Start()
     {
@@ -113,9 +21,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement = Vector2.zero;  // 매 프레임마다 초기화
+        movement = Vector2.zero;
 
-        if (isMovingUp)
+        // 사다리 영역에서만 위로 이동
+        if (isMovingUp && canMoveUp)
         {
             movement.y = 1;
         }
@@ -138,10 +47,29 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        // movement 값이 0이 아닐 때만 움직임 처리
         if (movement != Vector2.zero)
         {
             rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    // 사다리 영역에 들어왔을 때
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("ladder"))
+        {
+            canMoveUp = true;
+            Debug.Log("Entered ladder Area");
+        }
+    }
+
+    // 사다리 영역에서 나갔을 때
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("ladder"))
+        {
+            canMoveUp = false;
+            Debug.Log("Exited ladder Area");
         }
     }
 
