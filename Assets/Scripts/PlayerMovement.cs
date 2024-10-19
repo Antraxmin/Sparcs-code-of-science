3f616549e4f6e238e3c4fbebf320bbc8a5739521
Antraxmin <antraxmin@naver.com>
2024-10-19 15:29:54 +0900

    feat: 플레이어 이동 시 스프라이트 반전 기능 구현

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     private Vector2 movement;
-    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -18,7 +18,15 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Speed", movement.sqrMagnitude); 
+        if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true; 
+        }
+
+        else if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false; 
+        }
     }
 
     void FixedUpdate()
